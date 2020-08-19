8fcb3fcf555c17548665e2af75feeaaaac86ebf5
Irem Yigit <yigitiremm@gmail.com>
2020-08-19 15:19:51 +0300

    giris_1 adi degistirilip Toplama yapildi

--- src/Toplama.java
+++ src/Toplama.java
@@ -1,31 +1,21 @@
 import java.util.Locale;
 import java.util.Scanner;
 
-public class giris_1
+public class Toplama
 {
 
     public static void main(String[] args)
     {
         Scanner scan = new Scanner(System.in); //kullanıcıdan input alma
 
-        System.out.println("Bir sayi girin: ");
+        System.out.print("Bir sayi girin: ");
         int a=scan.nextInt(); //nextInt() kullanıcıdan int deger almak için kullanılır.
 
-        System.out.println("ikinci sayiyi girin: ");
+        System.out.print("ikinci sayiyi girin: ");
         int b=scan.nextInt();
 
-        //System.out.println("a degiskeni "+a+" dir");
-        //System.out.println("b degiskeni "+b+" dir");
         int toplam=a+b;
-
         System.out.println(toplam);
-
-
         scan.useLocale(Locale.US); //bu şekilde 3.14 yazabilirim.
-
-
-
-
-
     }
 }
