22e3aa53d7803e2798a8b120318a095686cb099b
Irem Yigit <yigitiremm@gmail.com>
2020-08-21 14:33:01 +0300

    AsalCarpanlaraAyirma kodu degistirildi

--- src/AsalCarpanlaraAyirma.java
+++ src/AsalCarpanlaraAyirma.java
@@ -3,7 +3,7 @@ public class AsalCarpanlaraAyirma {
     public static void main(String[] args){
 
         int  carpan=2;
-        long sayi = 600851475143L; //Javada uzun degismez degerler L ile biter.
+        long sayi = 60; //Javada uzun degismez degerler L ile biter.
 
         while (sayi/carpan!=1){
             if(sayi%carpan==0){
@@ -13,10 +13,6 @@ public class AsalCarpanlaraAyirma {
                 carpan=carpan+1;
             }
         }
-        System.out.print("sayi: "+sayi);
-
-
-
-
+        System.out.print("En büyük asal carpan: "+carpan);
     }
 }
